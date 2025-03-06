aba7901663846592c894e1be8e8b0510ca624ede
robertkutwa <robertkutwa0@gmail.com>
2025-03-06 15:14:31 +0300

    Functions Lab (CodeGrade)

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const headquarters = 42;
+    return Math.abs(someValue - headquarters);
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
+
+console.log(distanceFromHqInBlocks(50));
+console.log(distanceFromHqInFeet(50));
+console.log(calculatesFarePrice(34, 24));
+console.log(calculatesFarePrice(50, 58));
+console.log(calculatesFarePrice(34, 32));
+console.log(calculatesFarePrice(43, 44));
